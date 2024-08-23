de89164ada60eefbc7b1d31681df1c314c8207da
Sarah Habicht <sarah.habicht@tngtech.com>
2024-08-23 17:05:14 +0200

    Fix main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'Sarah H.',
+		name: 'James Butler',
 		// In the following fields you can either give a single string,
 		// or an array of bullet points
 		
